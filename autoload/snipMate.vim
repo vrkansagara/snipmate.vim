vrkansagara/snipmate.vim

commit cd4f1ed986fdce679583072b03d92ad582a18851

Use loops and globs instead of :runtime
In older versions of Vim (prior to 7.3.1295), if a trigger contains
certain characters (like $), Vim can take a while to evaluate the globs.
Thus we avoid :runtime entirely in favor of s:Glob from before the
override branch was merged. This ends up being a bit slower in recent
Vim versions but much faster in older versions. I think this trade-off
is acceptable.

This also avoids tampering with 'runtimepath', which we never should
have been doing in the first place. That was probably a big reason the
override branch wasn't merged for a long time.

See also #195.

autoload/snipMate.vim
@@ -331,7 +331,7 @@ function! s:snippet_filenames(scope, trigger) abort
 	let mid = ['', '_*', '/*']
 	let mid += map(copy(mid), "'/' . a:trigger . '*' . v:val")
 	call map(mid, "'snippets/' . a:scope . v:val . '.snippet'")
-	return join(map(mid[:2], 'v:val . "s"') + mid[3:])
+	return map(mid[:2], 'v:val . "s"') + mid[3:]
 endfunction
 
 function! snipMate#SetByPath(dict, trigger, path, snippet, bang, snipversion) abort
@@ -342,12 +342,27 @@ function! snipMate#SetByPath(dict, trigger, path, snippet, bang, snipversion) ab
 	let d[a:trigger][a:path] = [a:snippet, a:snipversion]
 endfunction
 
+if v:version < 704 || has('win32')
+	function! s:Glob(path, expr)
+		let res = []
+		for p in split(a:path, ',')
+			let h = split(fnamemodify(a:expr, ':h'), '/')[0]
+			if isdirectory(p . '/' . h)
+				call extend(res, split(glob(p . '/' . a:expr), "\n"))
+			endif
+		endfor
+		return filter(res, 'filereadable(v:val)')
+	endfunction
+else
+	function! s:Glob(path, expr)
+		return split(globpath(a:path, a:expr), "\n")
+	endfunction
+endif
+
 " default triggers based on paths
 function! snipMate#DefaultPool(scopes, trigger, result) abort
 	let scopes = s:AddScopeAliases(a:scopes)
 	let scopes_done = []
-	let rtp_save = &rtp
-	let &rtp = join(g:snipMate.snippet_dirs, ',')
 	let s:lookup_state = {}
 	let s:lookup_state.snips = []
 
@@ -356,7 +371,13 @@ function! snipMate#DefaultPool(scopes, trigger, result) abort
 		let s:lookup_state.scope = scope
 		let s:lookup_state.extends = []
 
-		exec 'runtime!' s:snippet_filenames(scope, escape(a:trigger, "*[]?{}`'$|#%"))
+		for expr in s:snippet_filenames(scope, escape(a:trigger, "*[]?{}`'$|#%"))
+			for path in g:snipMate.snippet_dirs
+				for file in s:Glob(path, expr)
+					source `=file`
+				endfor
+			endfor
+		endfor
 
 		call add(scopes_done, scope)
 		call extend(scopes, s:lookup_state.extends)
@@ -368,8 +389,6 @@ function! snipMate#DefaultPool(scopes, trigger, result) abort
 			call snipMate#SetByPath(a:result, trigger, desc, contents, bang, snipversion)
 		endif
 	endfor
-
-	let &rtp = rtp_save
 endfunction
 
 " return a dict of snippets found in runtimepath matching trigger

commit 599bc23fa9669c4d0b49262b3209242d1807e1e0

Fix SnipMateOpenSnippetFiles
This was broken by cd4f1ed986f.

Fixes #196.

autoload/snipMate.vim
@@ -410,10 +410,10 @@ function! snipMate#OpenSnippetFiles() abort
 	let exists = []
 	let notexists = []
 	for scope in s:AddScopeAliases(snipMate#ScopesByFile())
-		let files += split(s:snippet_filenames(scope, ''))
+		let files += s:snippet_filenames(scope, '')
 	endfor
 	call filter(files, "v:val !~# '\\*'")
-	for path in split(g:snipMate.snippet_dirs, ',')
+	for path in g:snipMate.snippet_dirs
 		let fullpaths = map(copy(files), 'printf("%s/%s", path, v:val)')
 		let exists += filter(copy(fullpaths), 'filereadable(v:val)')
 		let notexists += map(filter(copy(fullpaths),